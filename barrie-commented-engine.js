8d97c400f02341b8ae9d42dff46b2e3bb75f801f
Barrie Reader <me@barriereader.co.uk>
2020-02-09 02:29:01 +0000

    comments

--- barrie-commented-engine.js
+++ barrie-commented-engine.js
@@ -1,3 +1,4 @@
+//this creates a character template object which holds generic player information
 var characterTemplate = {
 	name: "",
 	hand: [],
@@ -13,58 +14,63 @@ var characterTemplate = {
 	}
 };
 
+//create a placeholder global variable to hold the "current player"
 var player = null;
 
+//this block of code runs as soon as the page is ready to go
 $(document).ready(function() {
 	
+	//set the player variable to be a copy of the character template object
 	player = characterTemplate;
 	
+	//the room cards
 	var roomCards = [
-    {
-        roomType: "Dark Dungeon Room",
-        text: "The room is damp dark and smells of decay.",
-        someEffect: 30,
-		treasureChance: 20,
-		randomMonster: 45
-    },
-    {
-        roomType: "Long well lit hallway",
-        text: "The hallway is barely long enough to spread your arms out to your sides",
-        someEffect: 0,
-		treasureChance: 20,
-		randonMonster: 45
-    },
-	{
-		roomType: "Dark Hallway",
-		text: "This hallway is so dark you cannot see your hand in front of your face",
-		someEffect: 40,
-		treasureChance: 20,
-		randomMonster: 45
-	},
-];
+		{
+			roomType: "Dark Dungeon Room",
+			text: "The room is damp dark and smells of decay.",
+			someEffect: 30,
+			treasureChance: 20,
+			randomMonster: 45
+		},
+		{
+			roomType: "Long well lit hallway",
+			text: "The hallway is barely long enough to spread your arms out to your sides",
+			someEffect: 0,
+			treasureChance: 20,
+			randonMonster: 45
+		},
+		{
+			roomType: "Dark Hallway",
+			text: "This hallway is so dark you cannot see your hand in front of your face",
+			someEffect: 40,
+			treasureChance: 20,
+			randomMonster: 45
+		},
+	];
 	
+	//The monster cards
 	var monsterCards = [
-	{
-		monsterType: "Group of Snotlings",
-		text: "These small green creatures bare, sharp teeth as they look at you",
-		attackDmg: "d4",
-		armourRating: 0,
-		specialAttack: 20
-	},
-	{
-		monsterType: "goblin scout",
-		text: "A goblin spots you",
-		attackDmg: "d4",
-		armourRating: 1,
-		specialAttack: 20
-	},
-	{
-		monsterType: "Reanimated corpse",
-		text: "A corpse of a fallen hero reanimates and moves towards you",
-		attackDmg: "d4",
-		armourRating: 1,
-		specialAttack: 20
-	},
+		{
+			monsterType: "Group of Snotlings",
+			text: "These small green creatures bare, sharp teeth as they look at you",
+			attackDmg: "d4",
+			armourRating: 0,
+			specialAttack: 20
+		},
+		{
+			monsterType: "goblin scout",
+			text: "A goblin spots you",
+			attackDmg: "d4",
+			armourRating: 1,
+			specialAttack: 20
+		},
+		{
+			monsterType: "Reanimated corpse",
+			text: "A corpse of a fallen hero reanimates and moves towards you",
+			attackDmg: "d4",
+			armourRating: 1,
+			specialAttack: 20
+		},
 	];
 		
 	// click function to fade out intro button&title
@@ -78,20 +84,31 @@ $(document).ready(function() {
 		}, 500);
 	});
 	
+	
+	//click function to fade out player classes and start game
 	$(".playerClass").on("click", function(e) {
 		e.preventDefault();
 		
+		//prompt for player's name
 		var myName = prompt("What is your name hero?")
+		
 		// added new error variable, zero is no error
 		var error = 0;
+		
 		//setting player name
 		player.name = myName;
-		//check if the player.name length is under one characterSet
-		//if it IS under one character, then set "error to "1"
+		
+		//check if the player.name does NOT exist
 		if (!player.name) {
+			
+			//does not exist, set error to 1
 			error = 1;
 		} else {
+			
+			//player.name exists, check the length
 			if (player.name.length < 1) {
+				
+				//length is less than 1 character, set error to 1
 				error = 1;
 			}
 		}
@@ -119,44 +136,57 @@ $(document).ready(function() {
 			player.stats.wis = 4;
 			player.stats.lck = 5;
 		}
+		
 		//we check if error is zero
 		//if the error IS zero, then we continue
 		//otherwise we alert() - "you didn't enter a name"
 		if (error == 0) {
 			
+			//fade out the player class selection
 			$("#classSelect").fadeOut(500);
 			
+			//set the title of the clicked card to be the player name
 			$(this).children(".title").html(player.name);
 		
+			//clone this card and add it to #theGame element
 			$(this).clone().appendTo("#theGame");
 		
+			//on the new cloned card, remove the playerClass and add a selectedPlayer class
 			$("#theGame .playerClass").removeClass("playerClass").addClass("selectedPlayer");
 			
+			//wait half a second then fade the game in
 			setTimeout(function() {
 				$("#theGame").fadeIn(500);
 			}, 500);
+			
 		} else {
+			
+			//the error variable was NOT "1"
 			alert("You didn't enter a name...");
 		}
 	});
 	
-		$("#rollDiceButton").on("click", function(e) {
-			e.preventDefault();
-			var output0 = createRandom("d4");
-			$(".dice0").html("<img src='images/dice" + output0 + ".png' />")
-		});
+	//the rolldice click function
+	$("#rollDiceButton").on("click", function(e) {
+		e.preventDefault();
+		var output0 = createRandom("d4");
+		$(".dice0").html("<img src='images/dice" + output0 + ".png' />")
+	});
 	
+	//the random creation function
 	var rollDice = function(min, max) {
 		var randomNumber = Math.floor(Math.random() * max) + min;
 		return randomNumber;
 	};
 	
+	//the actual d4,d6,etc dice roller
 	var createRandom = function(diceType) {
 		if (diceType == "d4") {
 			return rollDice(1, 4);
 		};
 	};
 		
+	//the healthbar animation
 	var runAnimation = function() {
 		
 		$("#healthBar .inner").animate({
@@ -168,9 +198,8 @@ $(document).ready(function() {
 		});
 		
 	};
-	
-	//merp
-	
+
+	//run the animation
 	runAnimation();
 	
 });
